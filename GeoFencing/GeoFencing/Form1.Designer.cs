261f83039f34459cbf62fbc27a2f916d83c239a4
jtwrigh56 <jtwright92@gmail.com>
2018-07-07 22:58:34 -0400

    Add files via upload

---
+++ GeoFencing/GeoFencing/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace GeoFencing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserGpsTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.polytextbox = new System.Windows.Forms.TextBox();
+            this.PointOfInterest = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UserGpsTextbox
+            // 
+            this.UserGpsTextbox.Location = new System.Drawing.Point(13, 13);
+            this.UserGpsTextbox.Name = "UserGpsTextbox";
+            this.UserGpsTextbox.Size = new System.Drawing.Size(213, 20);
+            this.UserGpsTextbox.TabIndex = 0;
+            this.UserGpsTextbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Users Location";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // polytextbox
+            // 
+            this.polytextbox.Location = new System.Drawing.Point(13, 39);
+            this.polytextbox.Multiline = true;
+            this.polytextbox.Name = "polytextbox";
+            this.polytextbox.Size = new System.Drawing.Size(213, 80);
+            this.polytextbox.TabIndex = 4;
+            this.polytextbox.Text = "43.10098, -82.66387\r\n43.11301, -82.49084\r\n42.95943, -82.66044\r\n42.98003, -82.4256" +
+    "1";
+            // 
+            // PointOfInterest
+            // 
+            this.PointOfInterest.Location = new System.Drawing.Point(13, 152);
+            this.PointOfInterest.Name = "PointOfInterest";
+            this.PointOfInterest.Size = new System.Drawing.Size(100, 20);
+            this.PointOfInterest.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(193, 155);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(32, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Validate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Radius Point";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Radius in Ft";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 229);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.PointOfInterest);
+            this.Controls.Add(this.polytextbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UserGpsTextbox);
+            this.Name = "Form1";
+            this.Text = "GeoFencing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserGpsTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox polytextbox;
+        private System.Windows.Forms.TextBox PointOfInterest;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GeoFencing/GeoFencing/Form1.cs
@@ -0,0 +1,211 @@
+﻿/*Authors: Mike Condino & James Wright
+ *Program: GeoFencing
+ *Details: This program will allow you to set a poly of your choicing and will compare with a single point to see if that point is in the polygon.
+ * the second half of this program validates whether a point is within the radius of a set point. 
+ * 
+ * Date: 7/7/2018
+ *
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoFencing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool inside = false;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*The First half was created by Mike This is validating the coordinates with the polygon. 
+         *
+         * */
+        
+        //Validation for the polygon. 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inside = false;
+
+            string[] GPSarr = UserGpsTextbox.Text.Split(new[] { ',', ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double Lat = double.Parse(GPSarr[0]);
+            double Lon = double.Parse(GPSarr[1]);
+
+            Point GPSPoint = new Point(Lat, Lon);
+
+            MessageBox.Show("Coordinates are: " + Lat + " " + Lon);
+
+            string[] Polyarr = polytextbox.Text.Split(new[] { ',', ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double PLat1 = double.Parse(Polyarr[0]);
+            double PLon1 = double.Parse(Polyarr[1]);
+            double PLat2 = double.Parse(Polyarr[2]);
+            double PLon2 = double.Parse(Polyarr[3]);
+            double PLat3 = double.Parse(Polyarr[4]);
+            double PLon3 = double.Parse(Polyarr[5]);
+            double PLat4 = double.Parse(Polyarr[6]);
+            double PLon4 = double.Parse(Polyarr[7]);
+           
+
+            var polygon = new List<Point> {
+            new Point(PLat1, PLon1),
+            new Point(PLat2, PLon2),
+            new Point(PLat3, PLon3),
+            new Point(PLat4, PLon4)
+            };
+
+            
+
+            PolyContainsPoint(polygon, GPSPoint);
+
+            if (inside == true)
+            {
+                MessageBox.Show("Point is inside the polygon");
+            }
+            else
+            {
+                MessageBox.Show("Point is not inside the polygon");
+            };
+        }
+
+        public bool PolyContainsPoint(List<Point> points, Point p)
+        {
+            Point v1 = points[points.Count - 1];
+
+            foreach (Point v0 in points)
+            {
+                double d1 = (p.Y - v0.Y) * (v1.X - v0.X);
+                double d2 = (p.X - v0.X) * (v1.Y - v0.Y);
+
+                if (p.Y < v1.Y)
+                {
+                    if (v0.Y <= p.Y)
+                    {
+                        if (d1 > d2)
+                        {
+                            inside = !inside;
+                        }
+                    }
+                }
+                else if (p.Y < v0.Y)
+                {
+                    if (d1 < d2)
+                    {
+                        inside = !inside;
+                    }
+                }
+
+                v1 = v0;
+            }
+
+            return inside;
+        }
+
+        /*The Second half was created by James. this validates a single point and determines whether a second point is within the radius of the first point. 
+         * */
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] GPSarr = UserGpsTextbox.Text.Split(new[] { ',', ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double lat = double.Parse(GPSarr[0]);
+            double lon = double.Parse(GPSarr[1]);
+
+            string[] POI = PointOfInterest.Text.Split(new[] { ',', ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double plat = double.Parse(POI[0]);
+            double plon = double.Parse(POI[1]);
+
+            double radius = double.Parse(textBox4.Text);
+
+            double w = (radius / 5280);
+
+            double distance = GetDistance(lat, lon, plat, plon);
+
+
+
+            checkRadius(distance, w);
+        }
+
+        //Checks whether the points distance is greater than the radius of the point that is set. 
+        public void checkRadius(double d, double r)
+        {
+
+            if (d < r)
+            {
+                MessageBox.Show("The Point is inside the Radius!");
+            }
+            else
+            {
+                MessageBox.Show("The Point is outside of the Radius!");
+            }
+
+
+        }
+
+
+        //Converts the Lat, and Lon to radians so the distance can be properly calculated. 
+        double GetDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            var R = 3958.756; // Radius of the earth in miles
+            var dLat = ToRadians(lat2 - lat1);  // deg2rad below
+            var dLon = ToRadians(lon2 - lon1);
+            var a =
+                Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ToRadians(lat1)) * Math.Cos(ToRadians(lat2)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            var d = R * c; // Distance in miles
+            return d;
+        }
+
+        //conversion to Radians
+        double ToRadians(double deg)
+        {
+            return deg * (Math.PI / 180);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+
+
+
+}
